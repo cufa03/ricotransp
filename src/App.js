55e7d2e9495fa02479df54d641645f01e0dc2e48
Facundo Nadaf <facundo.nadaf@integrify.io>
2023-03-20 16:01:35 +0100

    Finished nuestros camiones

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/index';
 import Home from './components/pages/home/Home';
 import AboutSection from './components/pages/about';
+import OurTrucks from './components/pages/trucks';
 import Footer from './components/footer/index';
 import ContactUs from './components/pages/contact';
 
@@ -12,7 +13,7 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" exact element={<Home />} />
-				<Route path="/trucks" exact></Route>
+				<Route path="/trucks" exact element={<OurTrucks />}></Route>
 				<Route path="/about-section" exact element={<AboutSection />}></Route>
 				<Route path="/contact" exact element={<ContactUs />} />
 			</Routes>
---
+++ src/components/pages/trucks/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import camion1 from '../../../assets/nuestros-camiones-1.jpg';
+import camion2 from '../../../assets/nuestros-camiones-2.jpg';
+import camion3 from '../../../assets/nuestros-camiones-3.jpg';
+import camion4 from '../../../assets/nuestros-camiones-4.jpg';
+import camion5 from '../../../assets/nuestros-camiones-5.jpg';
+import camion6 from '../../../assets/nuestros-camiones-6.jpg';
+import './styles.css';
+const ourTrucks = () => {
+	return (
+		<div>
+			<div className="titulo"></div>
+			<div className="gallery">
+				<figure class="gallery__item gallery__item--1">
+					<img src={camion1} class="gallery__img" alt="imagen de un camion" />
+				</figure>
+				<figure class="gallery__item gallery__item--2">
+					<img src={camion6} class="gallery__img" alt="imagen de un camion" />
+				</figure>
+				<figure class="gallery__item gallery__item--3">
+					<img src={camion3} class="gallery__img" alt="imagen de un camion" />
+				</figure>
+				<figure class="gallery__item gallery__item--4">
+					<img src={camion4} class="gallery__img" alt="imagen de un camion" />
+				</figure>
+				<figure class="gallery__item gallery__item--5">
+					<img src={camion5} class="gallery__img" alt="imagen de un camion" />
+				</figure>
+				<figure class="gallery__item gallery__item--6">
+					<img src={camion2} class="gallery__img" alt="imagen de un camion" />
+				</figure>
+			</div>
+		</div>
+	);
+};
+
+export default ourTrucks;
